b3f6e6ff3f5a7717de8df3cd94945c65a3e8825e
Kevin Stradtman <stradtkt22@gmail.com>
2021-01-13 22:35:36 -0500

    Added the html static and options files

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, redirect, url_for, request, make_response
 
 app = Flask(__name__)
 
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    data = get_saved_data()
+    return render_template('index.html', saves=data)
 
+@app.route('/builder')
+def builder():
+    return render_template('builder.html', saves=get_saved_data())
+
+@app.route('/save', methods=['POST'])
+def save():
+    response = make_response(redirect(url_for('index')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(data))
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8000)
